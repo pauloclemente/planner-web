115678bbaa26752b9fae95f2052d142ce5f8ab6c
Paulo Clemente <clementedbarros@gmail.com>
2024-10-08 11:11:25 -0300

    chore: update invite guests props

--- src/pages/create-tip/steps/invite-guests.tsx
+++ src/pages/create-tip/steps/invite-guests.tsx
@@ -5,6 +5,7 @@ interface InviteGuestsStepProps {
   openGuestsModal: () => void;
   openConfirmTripModal: () => void;
   emailsToInvite: string[];
+  setError: React.Dispatch<React.SetStateAction<string | null>>;
 }
 
 export function InviteGuestsStep({
